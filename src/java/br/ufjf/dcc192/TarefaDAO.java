aa4452f764f3a2017ab89519239ec5e162839625
Igor Knop <igorknop@gmail.com>
2018-05-07 20:15:17 -0300

    Adiciona comandos

---
+++ src/java/br/ufjf/dcc192/TarefaDAO.java
@@ -0,0 +1,25 @@
+package br.ufjf.dcc192;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TarefaDAO {
+
+    private static TarefaDAO instancia;
+
+    public static TarefaDAO getInstance() {
+        if (instancia == null) {
+            instancia = new TarefaDAO();
+        }
+        return instancia;
+    }
+
+    public List<String> listAll() {
+        List<String> tarefas = new ArrayList<>();
+        tarefas.add("Comprar ovos");
+        tarefas.add("Comprar farinha");
+        tarefas.add("Comprar açúcar");
+        return tarefas;
+    }
+
+}
---
+++ src/java/br/ufjf/dcc192/TarefasServlet.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufjf.dcc192;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ice
+ */
+@WebServlet(name = "TarefasServlet", urlPatterns = {"/index.html", "/tarefas.html", "/tarefa.html"})
+public class TarefasServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String> rotas = new HashMap<>();
+        rotas.put("/index.html", "br.ufjf.dcc192.IndexCommand");
+        rotas.put("/tarefas.html", "br.ufjf.dcc192.TarefasListCommand");
+        
+        String clazzName = rotas.get(request.getServletPath());
+        try {
+            Comando comando = (Comando) Class.forName(clazzName).newInstance();
+            comando.exec(request, response);
+        } catch (ClassNotFoundException|IllegalAccessException|InstantiationException ex) {
+            response.sendError(500, "Erro: "+ex);
+            Logger.getLogger(TarefasServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
